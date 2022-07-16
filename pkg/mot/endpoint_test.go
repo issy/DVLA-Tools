5190ccbb3db06f69704ea429593baabb2a2b3446
issy <issy@issy.dev>
2022-07-16 02:06:12 +0100

    Unname unused variable

--- pkg/mot/endpoint_test.go
+++ pkg/mot/endpoint_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestsEndpointReturnsData(t *testing.T) {
 	client := NewClient(os.Getenv("DVLA_MOT_API_KEY"))
-	car, err := client.GetCar("RJ06JOU")
+	_, err := client.GetCar("RJ06JOU")
 	if err != nil {
 		t.Fatal()
 	}
