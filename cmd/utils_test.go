e10b2c1b971950c8cda019f169dce3d92e13520c
issy <issy@issy.dev>
2022-07-16 02:07:43 +0100

    Run tests correctly

--- cmd/utils_test.go
+++ cmd/utils_test.go
@@ -2,14 +2,14 @@ package cmd
 
 import "testing"
 
-func TestsVESAPIKey(t *testing.T) {
+func TestVESAPIKey(t *testing.T) {
 	apiKey := readVESApiKeyFromEnvVar()
 	if apiKey == "" {
 		t.Fatal("API key not set")
 	}
 }
 
-func TestsMOTAPIKey(t *testing.T) {
+func TestMOTAPIKey(t *testing.T) {
 	apiKey := readMOTApiKeyFromEnvVar()
 	if apiKey == "" {
 		t.Fatal("API key not set")
--- pkg/mot/endpoint_test.go
+++ pkg/mot/endpoint_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestsEndpointReturnsData(t *testing.T) {
+func TestEndpointReturnsData(t *testing.T) {
 	client := NewClient(os.Getenv("DVLA_MOT_API_KEY"))
 	_, err := client.GetCar("RJ06JOU")
 	if err != nil {
